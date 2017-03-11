1b38a8ca7f4766705545794fef7a8dda63aba0d5
Slivinskas <slivinskasvytenis@gmail.com>
2017-03-11 13:27:36 +0200

    API

--- api.php
+++ api.php
@@ -5,3 +5,36 @@
  * Date: 2017-03-11
  * Time: 07:56
  */
+$config =  include_once ("config.php");
+include_once ("libary/DB.class.php");
+
+$db = new DB($config->host,$config->user,$config->pass,$config->db);
+
+/*
+ * $_GET["start"] // šepetėlio numeris
+ * return //valymo numeris
+ *
+ * */
+if(isset($_GET["start"])){
+    echo $db->setStart($_GET["start"]);
+    exit;
+}
+
+/*
+ * $_GET["endtime"] // valymo numeris
+ * $_GET["period"] // valymo trukmė
+ *
+ * */
+if(isset($_GET["endtime"])&&isset($_GET["period"])){
+    $db->setEnd($_GET["endtime"],$_GET["period"]);
+    echo "end";
+    exit;
+}
+
+// return // visi duomenys
+if(isset($_GET["getdata"])){
+    echo json_encode($db->getStatistic());
+    exit;
+}
+
+echo "Nieko";
